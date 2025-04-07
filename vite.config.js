6d4c6ad963e14282ee8f1731bd00b5e77d323c36
Josh Macaraig <hs0jmacaraig@gmail.com>
2025-04-07 09:25:18 +0800

    f

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/church-connect/', // Adds the repository name as the base path
+  base: './', // Use relative paths instead of absolute
   build: {
     outDir: 'dist',
     emptyOutDir: true,

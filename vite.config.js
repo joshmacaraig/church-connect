93a73cc42e5355cedebba4429cdbaa5539613c1e
Josh Macaraig <61573580+joshmacaraig@users.noreply.github.com>
2025-04-07 13:10:59 +0800

    build deply

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/church-connect/', // Set the base path explicitly to the subdirectory
+  base: '/church-connect/', // Set the base path to the subdirectory
   build: {
     outDir: 'dist',
     emptyOutDir: true,

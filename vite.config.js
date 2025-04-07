96928d79b0c059e015af3d56fdb6a1736cc24ce2
Josh Macaraig <61573580+joshmacaraig@users.noreply.github.com>
2025-04-07 09:37:43 +0800

    fixing 404

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: './', // Use relative paths instead of absolute
+  base: '/church-connect/', // Set the base path explicitly to the subdirectory
   build: {
     outDir: 'dist',
     emptyOutDir: true,

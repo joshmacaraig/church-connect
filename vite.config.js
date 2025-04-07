949f8b9cde2abc8e6040c3870bc6ff18c3dbc6a9
Josh Macaraig <hs0jmacaraig@gmail.com>
2025-04-07 09:04:43 +0800

    Add CNAME file and fix GitHub Pages configuration

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/church-connect/', // Adds the repository name as the base path for both GitHub Pages and custom domain
+  base: '/church-connect/', // Adds the repository name as the base path
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true,
+  }
 })
